291b7f189e063a8f023b640af5db1520b13fd18a
root@bilby <me@cypherpunk.org>
2018-03-21 22:53:49 +0100

    tag and a dict are mandatory on constructor

--- test_xml2bib.py
+++ test_xml2bib.py
@@ -6,8 +6,10 @@ import unittest
 
 
 class TestBibTeXWrite(unittest.TestCase):
+    verbose = False
+
     def test_empty(self):
-        w = xml2bib.BibTeXWriter()
+        w = xml2bib.BibTeXWriter('', dict())
         x = w.str()
         self.assertTrue(len(x) > 0)
 
@@ -16,10 +18,11 @@ class TestBibTeXWrite(unittest.TestCase):
              'year': 1905,
              'title': 'The photo-electric effect',
              'journal': 'Phys. Rev.'}
-        w = xml2bib.BibTeXWriter()
+        w = xml2bib.BibTeXWriter('AE05', d)
         x = w.str()
         self.assertTrue(len(x) > 0)
-
+        if (self.verbose):
+            print('testBasic:', x)
 
 if __name__ == '__main__':
     unittest.main()
--- xml2bib.py
+++ xml2bib.py
@@ -2,14 +2,27 @@
 
 from __future__ import print_function
 
+"""
+Note the big X, not a chi, in the class name.
+
+It is possible that  {, " or $ need escaping with backslash ('\').
+"""
+
 
 class BibTeXWriter():
-    def __init__(self, data=dict()):
+    def __init__(self, tag, data, entry_type='Misc'):
+        self.entry_type = entry_type
+        self.tag = tag
         self.data = data
 
     def str(self):
         lines = []
-        for k in self.data.keys():
-            lines.append('  {} = {}'.format(k, self.data[k]))
-        return '@Misc[{}]\{' + ',\n'.join(lines) + '\}'
-
+        for k in sorted(self.data):
+            v = self.data[k]
+            if isinstance(v, int):
+                value = int(v)
+            else:
+                value = '{' + v + '}'
+            lines.append('  {} = {}'.format(k, str(value)))
+        return '@{0}{{{1},\n'.format(self.entry_type, self.tag,) + \
+               ',\n'.join(lines) + '\n}'
